c1743f2dad05fd652b7c47aae18758abf0ccef79
adan291 <adanx05@gmail.com>
2016-04-01 12:41:09 +0200

    Implementado atributos para nombre y ambos apellidos para cojer el numero exacto de esas letras

---
+++ NameGenerator.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class NameGenerator here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NameGenerator
+{
+    public final static int NOMBRE = 3;
+    public final static int APE_PARTE1 = 2;
+    public final static int APE_PARTE2 = 3;
+    
+}
